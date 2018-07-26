e34fd6fe0cd68f51b5715393686e45e6d7ffb5ae
zhygallo <zhigallandrejj@gmail.com>
2018-07-26 15:16:58 +0200

    Data Generation. Combine two modalities.

--- data_gen.py
+++ data_gen.py
@@ -6,13 +6,15 @@ import numpy as np
 import nibabel as nib
 
 
-def get_2d_patches(input_fold, test_img='', drop_class = [], normilize_per_case=True, output_fold=''):
+def get_2d_patches(input_fold, test_img='', drop_class = [], flair=False, t1=True, normilize_per_case=True, output_fold=''):
     """
     Generating 2d patches based on 3d input images.
     Args:
         input_fold: string. Path to the folder with two other folders: images and masks.
                             In turn raw 3d images and masks are stored respectively.
         drop_class: list of integers. List of classes that will be reasigned to background (class 0).
+        flair: boolean. True when flair images are used, False in opposite case.
+        t1: boolean. True when t1 images are used, False in opposite case.
         normilize_per_case: boolean. True when each image per case (3d image) is normilized, False if not.
         test_imgs:  string. Name of the image which is selected for testing among all images.
         output_fold: string. Path to the output folder with numpy files of images and masks.
@@ -20,6 +22,8 @@ def get_2d_patches(input_fold, test_img='', drop_class = [], normilize_per_case=
     Returns:
         tuple with 4 numpy files. 1.train images, 2.train masks, 3.test images, 4.test masks.
     """
+    assert flair or t1
+
     assert os.path.isdir(input_fold)
     if output_fold != '':
         assert os.path.isdir(output_fold)
@@ -29,7 +33,18 @@ def get_2d_patches(input_fold, test_img='', drop_class = [], normilize_per_case=
     if input_fold[-1] != '/':
         input_fold += '/'
 
-    img_files = os.listdir(os.path.join(input_fold, 'images'))
+    if flair:
+        flair_files = os.listdir(os.path.join(input_fold+"images/", 'FLAIR'))
+    if t1:
+        t1_img_files = os.listdir(os.path.join(input_fold + "images/", 'T1'))
+
+    if flair and t1:
+        assert len(flair_files) == len(t1_img_files)
+        img_files = flair_files
+    elif flair:
+        img_files = flair_files
+    elif t1:
+        img_files = t1_img_files
 
     train_imgs = []
     train_masks = []
@@ -41,28 +56,50 @@ def get_2d_patches(input_fold, test_img='', drop_class = [], normilize_per_case=
         if img_file != test_img:
             is_train_img = True
 
-        img = nib.load(os.path.join(input_fold, 'images/' + img_file)).get_data()
-        mask = nib.load(os.path.join(input_fold, 'masks/' + img_file.split('.')[0] + '_mask.nii')).get_data()
+        img = None
+        if flair:
+            flair_img = nib.load(os.path.join(input_fold, 'images/FLAIR/' + img_file.split('_')[0]+'_FLAIR.nii')).get_data()
+            flair_img = np.nan_to_num(flair_img)
+            if normilize_per_case:
+                mean = np.mean(flair_img)
+                std = np.std(flair_img)
+                flair_img -= mean
+                flair_img /= std
+            flair_img = flair_img[..., np.newaxis]
+            img = flair_img
+        if t1:
+            t1_img = nib.load(os.path.join(input_fold, 'images/T1/' + img_file.split('_')[0]+'_T1.nii')).get_data()
+            t1_img = np.nan_to_num(t1_img)
+            if normilize_per_case:
+                mean = np.mean(t1_img)
+                std = np.std(t1_img)
+                t1_img -= mean
+                t1_img /= std
+            t1_img = t1_img[..., np.newaxis]
+            img = t1_img
 
-        assert img.shape == mask.shape
+        if flair and t1:
+            img = np.concatenate((flair_img, t1_img), axis=3)
 
-        img = np.nan_to_num(img)
+        mask = nib.load(os.path.join(input_fold, 'masks/' + img_file.split('_')[0] + '_mask.nii')).get_data()
+
+        # img = np.nan_to_num(img)
         mask = np.nan_to_num(mask)
 
         for elem in drop_class:
             mask[mask == elem] = 0
 
-        if normilize_per_case:
-            mean = np.mean(img)
-            std = np.std(img)
-            img -= mean
-            img /= std
+        # if normilize_per_case:
+        #     mean = np.mean(img)
+        #     std = np.std(img)
+        #     img -= mean
+        #     img /= std
 
         for layer in range(img.shape[2]):
             patch_img = img[:, :, layer]
             patch_mask = mask[:, :, layer]
-            patch_img = np.reshape(patch_img, (patch_img.shape[0], patch_img.shape[1], 1))
-            patch_mask = np.reshape(patch_mask, (patch_mask.shape[0], patch_mask.shape[1], 1))
+            # patch_img = np.reshape(patch_img, (patch_img.shape[0], patch_img.shape[1], 1))
+            # patch_mask = np.reshape(patch_mask, (patch_mask.shape[0], patch_mask.shape[1], 1))
             if is_train_img:
                 train_imgs.append(patch_img)
                 train_masks.append(patch_mask)
@@ -94,7 +131,7 @@ def get_2d_patches(input_fold, test_img='', drop_class = [], normilize_per_case=
 @click.option('--normilize_per_case', default=True, type=click.BOOL )
 @click.option('--output_fold', default='', type=click.STRING, help='Path to the output folder where numpy arrays will be stored')
 def main(input_fold, test_img, normilize_per_case, output_fold):
-    get_2d_patches(input_fold=input_fold, normilize_per_case=normilize_per_case, drop_class=[], test_img=test_img, output_fold=output_fold)
+    get_2d_patches(input_fold=input_fold, normilize_per_case=normilize_per_case, drop_class=[9, 10], test_img=test_img, output_fold=output_fold)
 
 
 if __name__ == '__main__':
--- tools/visulization.py
+++ tools/visulization.py
@@ -8,7 +8,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def visualize_test_evaluation(input_file, output_folder):
+def visualize_test_evaluation(input_file, output_folder, experiment_title = ''):
     assert os.path.isfile(input_file)
 
     with open(input_file) as infile:
@@ -25,14 +25,17 @@ def visualize_test_evaluation(input_file, output_folder):
     x_axis = list(range(0, len(dsc_avg_list)*10, 10))
 
     plt.plot(x_axis, dsc_avg_list, 'r', x_axis, vs_avg_list, 'g')
-    plt.title('Average Dice Coefficient and Volume similarity')
+    plt.title('Average Dice Coefficient and Volume similarity \n' + experiment_title)
     plt.legend(['Avg. Dice Coefficient', 'Avg. Volume Similarity'])
+    plt.text(540, 0.25, 'max. dice coefficient: ' + str(np.max(dsc_avg_list)), verticalalignment='center')
+    plt.text(540, 0.2, 'max. volume similarity: ' + str(np.max(vs_avg_list)))
     plt.savefig(os.path.join(output_folder, 'dice_vs_graph.png'))
     plt.close()
 
     plt.plot(x_axis, h95_avg_list, 'b')
-    plt.title('Average Hausdorff distance')
+    plt.title('Average Hausdorff distance \n' + experiment_title)
     plt.legend(['Avg. Housdorff distance'])
+    # plt.text(0.1, 0.6, 'min. Avg. Housdorff distance: ' + str(np.min(dsc_avg_list)))
     plt.savefig(os.path.join(output_folder, 'h95.png'))
     plt.close()
 
@@ -41,8 +44,9 @@ def visualize_test_evaluation(input_file, output_folder):
 @click.command()
 @click.argument('input_file', type=click.STRING)
 @click.argument('output_folder', type=click.STRING)
-def main(input_file, output_folder):
-    visualize_test_evaluation(input_file, output_folder)
+@click.option('--experiment_title', type=click.STRING, default='')
+def main(input_file, output_folder, experiment_title=''):
+    visualize_test_evaluation(input_file, output_folder, experiment_title=experiment_title)
     return 0
 
 if __name__ == '__main__':
